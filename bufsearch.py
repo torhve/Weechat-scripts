f1d9b30fdc2bd1f1cfcb19191f5d42f24ba186cd
Tor Hveem <xt@sheeana.(none)>
2009-06-10 15:51:36 +0200

    Search in log file

--- bufsearch.py
+++ bufsearch.py
@@ -25,6 +25,7 @@
 # 2009-05-24, xt <xt@bash.no>
 #     version 0.1: initial release
 
+from __future__ import with_statement # This isn't required in Python 2.6
 import weechat as w
 import re
 weechat = w
@@ -36,23 +37,18 @@ SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "Search in buffer"
 SCRIPT_COMMAND = 'grep'
 
-# script options
-#settings = {
-#    "title_priority"       : '2',
-#    }
 
-#hooks = (
-#        'buffer_switch',
-#        'hotlist_*',
-#)
+
+def buffer_input(*kwargs):
+    return w.WEECHAT_RC_OK
+
+def buffer_close(*kwargs):
+    global search_buffer
+    search_buffer =  None
+    return w.WEECHAT_RC_OK
 
 if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
                     SCRIPT_DESC, "", ""):
-#    for option, default_value in settings.iteritems():
-#        if weechat.config_get_plugin(option) == "":
-#            weechat.config_set_plugin(option, default_value)
-#    for hook in hooks:
-#        weechat.hook_signal(hook, 'update_title', '')
     w.hook_command(SCRIPT_COMMAND,
                          "Buffer searcher",
                          "[expression]",
@@ -61,25 +57,73 @@ if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
                          "bufsearch_cmd",
                          "")
 
-def bufsearch_cmd(data, buffer, args):
-    #cb = weechat.current_buffer()
+search_buffer = w.buffer_new(SCRIPT_NAME, "buffer_input", "", "buffer_close", "")
+w.buffer_set(search_buffer, "type", "free")
+w.buffer_set(search_buffer, "title", "Search output buffer")
 
-    matcher = re.compile(args)
+def print_buffer(matching_lines):
+    w.buffer_set(search_buffer, "title", "Search matched %s lines" % len(matching_lines) )
+    for y, line in enumerate(matching_lines):
+        weechat.prnt_y(search_buffer, y, '%s %s%s %s' % (\
+            line[0],
+            line[1],
+            w.color('reset'),
+            line[2]))
 
+def find_infolist_matching_lines(buffer, matcher):
+    matching_lines = []
     infolist = w.infolist_get('buffer_lines', buffer, '')
-    channel =  w.buffer_get_string(buffer, 'name')
     while w.infolist_next(infolist):
         message = w.infolist_string(infolist, 'message')
         prefix = w.infolist_string(infolist, 'prefix')
         if matcher.search(message) or matcher.search(prefix):
-            w.prnt('', '%s %s%s %s' %(\
+            matching_lines.append((
                 w.infolist_time(infolist, 'date'),
                 w.infolist_string(infolist, 'prefix'),
-                w.color('reset'),
                 w.infolist_string(infolist, 'message'),
                 ))
 
     w.infolist_free(infolist)
-    return weechat.WEECHAT_RC_OK
+
+    return matching_lines
+
+
+def bufsearch_cmd(data, buffer, args):
+
+    if not args:
+        w.command('', '/help %s' %SCRIPT_COMMAND)
+        return w.WEECHAT_RC_OK
+
+    linfolist = w.infolist_get('logger_buffer', '', '')
+    logfilename = ''
+    log_enabled = False
+    while w.infolist_next(linfolist):
+        bpointer = w.infolist_pointer(linfolist, 'buffer')
+        if bpointer == buffer:
+            logfilename = w.infolist_string(linfolist, 'log_filename')
+            log_enabled = w.infolist_integer(linfolist, 'log_enabled')
+            break
+    w.infolist_free(linfolist)
+
+
+    matcher = re.compile(args, re.IGNORECASE)
+    matching_lines = []
+
+    if log_enabled:
+        with file(logfilename, 'r') as f:
+            for line in f:
+                if matcher.search(line):
+                    matching_lines.append(line.split('\t'))
+    else:
+        matching_lines = find_infolist_matching_lines(buffer, matcher)
+
+
+    if not matching_lines:
+        matching_lines = (('', '', 'No matches.'),)
+    print_buffer(matching_lines)
+
+    w.buffer_set(search_buffer, "display", "1")
+
+    return w.WEECHAT_RC_OK
 
 
